799a9151b6290254915d7c9eb8c3ed9c62a19ccb
Sergio <serrosan@hotmail.com>
2026-09-07 16:35:22 +0200

    Fix: no detener el arranque si view:cache falla (error no crítico)

--- docker/start-production.sh
+++ docker/start-production.sh
@@ -4,7 +4,7 @@ set -e
 php artisan storage:link || true
 php artisan config:cache
 php artisan route:cache
-php artisan view:cache
+php artisan view:cache || true
 php artisan migrate --force
 
 exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf

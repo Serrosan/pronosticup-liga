44febe617b28a325322bd2fde1b14b325db815b8
Sergio <serrosan@hotmail.com>
2026-09-16 12:37:44 +0200

    Fix: la carpeta 'cartas' no estaba permitida al subir imágenes

--- app/Http/Controllers/Api/V1/Admin/ImagenAdminController.php
+++ app/Http/Controllers/Api/V1/Admin/ImagenAdminController.php
@@ -19,7 +19,7 @@ class ImagenAdminController extends Controller
                 'max:3072',
                 'dimensions:min_width=50,min_height=50,max_width=4000,max_height=4000',
             ],
-            'carpeta' => ['required', 'string', 'in:ligas,arbitros,trofeos,jugadores,equipos,entrenadores,estadios'],
+            'carpeta' => ['required', 'string', 'in:ligas,arbitros,trofeos,jugadores,equipos,entrenadores,estadios,cartas'],
         ]);
 
         $nombreSeguro = Str::random(40).'.'.$request->file('imagen')->getClientOriginalExtension();
